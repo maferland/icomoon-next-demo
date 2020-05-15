5661c9dbee2454adbeb2c846bb0f1fdbf1208feb
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-05-14 20:50:20 -0400

    :bug: Fix leftover

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   webpack(config) {
-    config.resolve.alias["~"] = path.resolve(__dirname);
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|svg)$/,
       loader: "url-loader?limit=100000",
